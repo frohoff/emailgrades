45f4b9257539f7d4ac7348b2f6f905a528fa8610
Chris Frohoff <chris@Chriss-MacBook-Pro.local>
2016-12-06 19:01:29 -0800

    support multiple to addresses, fix for docker 1.2 quirks

--- emailgrades.sh
+++ emailgrades.sh
@@ -7,5 +7,7 @@ markup=$(echo "$output" | grep -Ev '^.\[')
 logs=$(echo "$output" | grep -E '^.\[')
 echo "$logs" >&2
 grades=$(echo "$markup" | grep grade_summary | grep -Eo "[^<>]{30,}" | sed -E 's/\.[0-9]{2}|%//g')
-email=$(printf "MIME-Version: 1.0\nContent-Type: text/html\nTo: $TO\nFrom: $FROM\nSubject: Grades: "; echo "$grades"; echo; echo '<html><body>'; echo "$markup"; echo '</body></html>') 
-echo "$email" | curl --url "smtp://smtp.cox.net" --ssl-reqd --mail-from "$FROM" --mail-rcpt "$TO" --upload-file - -v
+for to in $(echo $TO | tr ";,:" " "); do
+	email=$(printf "MIME-Version: 1.0\nContent-Type: text/html\nTo: $to\nFrom: $FROM\nSubject: Grades: "; echo "$grades"; echo; echo '<html><body>'; echo "$markup"; echo '</body></html>') 
+	echo "$email" | curl --url "smtp://smtp.cox.net" --ssl-reqd --mail-from "$FROM" --mail-rcpt "$to" --upload-file - -v
+done
